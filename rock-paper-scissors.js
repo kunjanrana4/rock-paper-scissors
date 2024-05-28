ed3e2c7b1e66b0052ff0837c7375a0da8ba2c257
Kunjan <kunjanrana4@gmail.com>
2024-05-28 10:57:58 -0400

    Feat: write computer choice logic
    Added and tested computer choice logic where the getComputerChoice() function returns either of the choices as the result.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+function getRandomInt(max) {
+    //return random number between 0, 1 and 2
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+    //randomly return "rock", "paper" or "scissors"
+    let randomNumber = getRandomInt(3);
+    let computerChoice = (randomNumber === 0) ? "rock" : (randomNumber === 1) ? "paper" : "scissors";
+    return computerChoice;
+} 
