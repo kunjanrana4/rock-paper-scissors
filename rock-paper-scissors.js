7b5a55333693c304daeeacc86ae052c4dcc19291
Kunjan <kunjanrana4@gmail.com>
2024-05-28 11:13:41 -0400

    feat: Write human choice logic
    Added logic for human choice by prompting to enter number between 0 to 2 to select rock, paper or scissors.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,7 @@
+function getChoice(number){
+    return (number === 0) ? "rock" : (number === 1) ? "paper" : "scissors";
+}
+
 function getRandomInt(max) {
     //return random number between 0, 1 and 2
     return Math.floor(Math.random() * max);
@@ -9,3 +13,10 @@ function getComputerChoice() {
     let computerChoice = (randomNumber === 0) ? "rock" : (randomNumber === 1) ? "paper" : "scissors";
     return computerChoice;
 } 
+
+function getHumanChoice() {
+    //ask user for a choice and return it
+    let humanChoice = getChoice(parseInt(prompt("Please enter a number between 0 to 2 to enter your choice.\n 0: rock\n1: paper\n2: scissors")));
+    console.log(humanChoice);
+    return humanChoice;
+}
